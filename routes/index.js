ddfc2f4084e1db0c02f572dfdaa780ed77b8065a
Drew Cain <drew@simpli.fi>
2016-01-24 14:39:59 -0600

    fixed brokenness, no functions are allowed in handlebars templates

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', app: express() });
+  var env = express().get('env');
+  res.render('index', { title: 'Express', env: env });
 });
 
 module.exports = router;
