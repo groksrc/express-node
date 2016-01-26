d284c2c80471225f561dc4667c43cc6e5646b021
Drew Cain <drew@simpli.fi>
2016-01-25 18:56:00 -0600

    added config info

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var consolidate = require('consolidate');
+var config = require('./config.json');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -26,6 +27,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next){
+    req.config = config;
+    next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var env = express().get('env');
-  res.render('index', { title: 'Express', env: env, func: function() { 
+  var app = express(),
+      env = app.get('env');
+  
+  res.render('index', { title: 'Express', env: env, myVal: req.config.myVal, func: function() { 
     var someVal = 1;
     return someVal;    
   } });
